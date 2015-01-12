23b914c6fc19dc8ede884172f83285eaf6ad1d04
Tam Nguyen <nguyentamvinhlong@gmail.com>
2015-01-12 09:50:05 +0700

    update

--- wp-config.php
+++ wp-config.php
@@ -78,3 +78,8 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST']);
+define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST']);
+//define('WP_ALLOW_MULTISITE', true);
+
